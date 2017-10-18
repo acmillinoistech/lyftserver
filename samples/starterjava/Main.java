ef0697bb52db3f1e8062fe4e81659de8f53914f2
Vinesh Kannan <vingkan@gmail.com>
2017-10-18 21:02:37 +0000

    Add basic unirest Java example

--- samples/starterjava/Main.java
+++ samples/starterjava/Main.java
@@ -1,23 +1,47 @@
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.*;
 
 /*
  * RUN
- * >> javac -cp ".:lib/*" SimpleServer.java
- * >> java -cp ".:lib/*" SimpleServer
- * Open: http://{workspacename}-{username}.c9users.io/{route}
+ * >> javac -cp ".:lib/*" Main.java
+ * >> java -cp ".:lib/*" Main
  */
  
 public class Main {
+	
+	public static String BASE_URL = "https://lyft-vingkan.c9users.io";
+	public static String TEAM = "your-team";
 
 	public Main() {
 	
 	}
 	
 	public static void main(String[] args) {
-		HttpResponse<String> response = Unirest.get("https://lyft-vingkan.c9users.io/trips/?start=9%2F12%2F17%202%3A00%20PM&end=9%2F12%2F17%203%3A00%20PM&team=team2")
-			.header("cache-control", "no-cache")
-			.header("postman-token", "3def01a5-99cc-6608-69d4-cc230b9bfe65")
-			.asString();
-		System.out.println(response);
+		
+		try {
+			
+			HttpResponse<String> request = Unirest.get(BASE_URL + "/trips/")
+				.queryString("team", TEAM)
+				.queryString("start", "9/10/2017 2:00 PM")
+				.queryString("end", "9/10/2017 3:00 PM")
+				.asString();
+			
+			System.out.println(request);
+			JSONObject response = new JSONObject(request.getBody());
+			JSONArray results = (JSONArray) response.getJSONArray("response");
+			for (int i = 0; i < results.length(); i++) {
+				JSONObject trip = (JSONObject) results.getJSONObject(i);
+				String pickup = (String) trip.get("pickup_community_area");
+				String dropoff = (String) trip.get("dropoff_community_area");
+				String out = String.format("Trip from area %s to area %s.", pickup, dropoff);
+				System.out.println(out);
+			}
+		
+		} catch (UnirestException ue) {
+			ue.printStackTrace();
+		}
+		
 	}
 
 }
