e4423aba91512d4c38a419d4a88a6d8242e7e6af
Vinesh Kannan <vingkan@gmail.com>
2017-10-19 00:44:07 +0000

    Modify python and java starters to match

--- samples/starterjava/Main.java
+++ samples/starterjava/Main.java
@@ -1,26 +1,26 @@
+/*
+ * Java Starter Code
+ * >> javac -cp ".:lib/*" Main.java
+ * >> java -cp ".:lib/*" Main
+ */
+
 import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-
-/*
- * RUN
- * >> javac -cp ".:lib/*" Main.java
- * >> java -cp ".:lib/*" Main
- */
  
 public class Main {
 	
 	public static final String BASE_URL = "https://lyft-vingkan.c9users.io";
 	public static final String TEAM = "your-team";
-
-	public Main() {
-	
-	}
 	
 	public static void main(String[] args) {
+		
+		/*
+		 * Example Usage
+		 */
 		
 		try {
 			
@@ -30,13 +30,24 @@ public class Main {
 			for (int i = 0; i < results.length(); i++) {
 				JSONObject trip = (JSONObject) results.getJSONObject(i);
 				String tripStart = (String) trip.get("trip_start_timestamp");
-				Date d = getDateFromString(tripStart);
+				Date d = stringToDate(tripStart);
 				String time = sdf.format(d);
 				String pickup = (String) trip.get("pickup_community_area");
 				String dropoff = (String) trip.get("dropoff_community_area");
 				String out = String.format("Trip at %s from area %s to %s", time, pickup, dropoff);
 				System.out.println(out);
 			}
+			
+			double base = 3.40;
+			double pickup = 1.00;
+			double perMile = 0.20;
+			double perMinute = 0.30;
+			JSONObject p = setPricing(base, pickup, perMile, perMinute);
+			System.out.println(p);
+			
+			int[] zones = {5, 6, 7};
+			JSONObject z = setZones(zones);
+			System.out.println(z);
 		
 		} catch (UnirestException ue) {
 			ue.printStackTrace();
@@ -46,6 +57,11 @@ public class Main {
 		
 	}
 	
+	/*
+	 * Helper Methods
+	 */
+	
+	// Get Trips
 	public static JSONObject getTrips(String start, String end) throws UnirestException {
 		HttpResponse<String> request = Unirest.get(BASE_URL + "/trips/")
 			.queryString("team", TEAM)
@@ -56,6 +72,7 @@ public class Main {
 		return response;
 	}
 	
+	// Get Trips (with limit)
 	public static JSONObject getTrips(String start, String end, int limit) throws UnirestException {
 		HttpResponse<String> request = Unirest.get(BASE_URL + "/trips/")
 			.queryString("team", TEAM)
@@ -67,7 +84,38 @@ public class Main {
 		return response;
 	}
 	
-	public static Date getDateFromString(String dateString) throws ParseException {
+	// Set Pricing
+	public static JSONObject setPricing(double base, double pickup, double perMile, double perMinute) throws UnirestException {
+		HttpResponse<String> request = Unirest.post(BASE_URL + "/pricing/")
+			.queryString("team", TEAM)
+			.queryString("base", base)
+			.queryString("pickup", pickup)
+			.queryString("per_mile", perMile)
+			.queryString("per_minute", perMinute)
+			.asString();
+		JSONObject response = new JSONObject(request.getBody());
+		return response;
+	}
+	
+	// Set Power Zones
+	public static JSONObject setZones(int[] zones) throws UnirestException {
+		String zoneList = "";
+		for (int i = 0; i < zones.length; i++) {
+			zoneList += zones[i];
+			if (i < (zones.length - 1)) {
+				zoneList += ",";
+			}
+		}
+		HttpResponse<String> request = Unirest.post(BASE_URL + "/zones/")
+			.queryString("team", TEAM)
+			.queryString("zones", zoneList)
+			.asString();
+		JSONObject response = new JSONObject(request.getBody());
+		return response;
+	}
+	
+	// Convert String to Date
+	public static Date stringToDate(String dateString) throws ParseException {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 		Date d = df.parse(dateString);
 		return d;
--- samples/starterpython/main.py
+++ samples/starterpython/main.py
@@ -1,5 +1,10 @@
+'''
+Python Starter Code
+>> python main.py
+'''
+
 import requests
-import datetime
+from datetime import datetime
 
 URL = "https://lyft-vingkan.c9users.io"
 TEAM = "your-team"
@@ -8,12 +13,19 @@ TEAM = "your-team"
 Helper Methods
 '''
 
+# Get Trips
+def get_trips(query):
+	query["team"] = TEAM
+	response = requests.get(URL + "/trips/", params=query)
+	return response.json()
+
+
 # Set Pricing
 def set_pricing(pricing):
 	query = pricing;
 	query["team"] = TEAM;
 	response = requests.post(URL + "/pricing/", params=query)
-	return response
+	return response.json()
 
 # Set Power Zones
 def set_zones(zones):
@@ -23,30 +35,36 @@ def set_zones(zones):
 		"zones": zone_list
 	}
 	response = requests.post(URL + "/zones/", params=query)
-	return response
-
-def get_trips(query):
-	query["team"] = TEAM
-	response = requests.get(URL + "/trips/", params=query)
-	res = response.json()
-	return res
+	return response.json()
+	
+def string_to_date(datestring):
+	return datetime.strptime(datestring, "%Y-%m-%dT%H:%M:%S")
 
 '''
 Example Usage
 '''
 
-set_zones([5, 6, 7])
+trips = get_trips({
+	"start": "9/10/2017 2:00 PM",
+	"end": "9/10/2017 3:00 PM",
+	"limit": 10
+})
 
-set_pricing({
+for trip in trips["response"]:
+	d = string_to_date(trip["trip_start_timestamp"])
+	time = d.strftime("%m/%d %r")
+	pickup = trip["pickup_community_area"]
+	dropoff = trip["dropoff_community_area"]
+	print("Trip at %s from area %s to %s" % (time, pickup, dropoff))
+
+p = set_pricing({
 	"base": 3.40,
 	"pickup": 1.00,
 	"per_mile": 0.20,
 	"per_minute": 0.30
 })
 
-data = get_trips({
-	"start": "9/10/2017",
-	"end": "9/11/2017"
-})
+print(p)
 
-print(data["length"])
+z = set_zones([5, 6, 7])
+print(z)
